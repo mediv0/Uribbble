3884761a2ada18ad50f834a91a3a09f3f3acd76a
mediv0 <anonymousaxis@gmail.com>
2021-11-16 20:48:47 +0330

    fix: observing attribute cause duplication on inject

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ const callback: MutationCallback = function (mutationsList, observer) {
         for (let i = 0; i < mutationsList.length; i++) {
                 // we check if comment section is fully loaded, we inject our comments
                 // this will cause layout shift in the page ( PERFORMANCE ISSUE )
-                if (mutationsList[i].target.nodeName === "TEXTAREA") {
+                if (
+                        mutationsList[i].previousSibling &&
+                        (mutationsList[i].previousSibling as HTMLDivElement)
+                                .className === "shot-comments-list"
+                ) {
                         injectComments();
                         break;
                 }
@@ -14,5 +18,5 @@ const callback: MutationCallback = function (mutationsList, observer) {
 
 // Create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
-const config = { attributes: true, childList: true, subtree: true };
+const config = { attributes: false, childList: true, subtree: true };
 observer.observe(document.body, config);
