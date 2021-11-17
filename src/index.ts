db55c9cd7ce615eee441b95eb3acdd17f6eaf4e4
mediv0 <anonymousaxis@gmail.com>
2021-11-17 18:32:38 +0330

    revert: duplication bug (3884761)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import { injectComments } from "./helpers";
 
-const callback: MutationCallback = function (mutationsList, observer) {
+const callback: MutationCallback = function (mutationsList, _observer) {
         // Use traditional 'for loops' for IE 11
         for (let i = 0; i < mutationsList.length; i++) {
                 // we check if comment section is fully loaded, we inject our comments
                 // this will cause layout shift in the page ( PERFORMANCE ISSUE )
                 if (
-                        (mutationsList[i].target as HTMLDivElement)
-                                .className === "sidebar-scrolling-container" &&
-                        (mutationsList[i].addedNodes[0] as HTMLDivElement)
-                                .className === "shot-sidebar-contents"
+                        mutationsList[i].target.nodeName === "TEXTAREA" &&
+                        mutationsList[i].attributeName === "data-tribute"
                 ) {
                         injectComments();
                         break;
@@ -19,5 +17,5 @@ const callback: MutationCallback = function (mutationsList, observer) {
 
 // Create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
-const config = { attributes: false, childList: true, subtree: true };
+const config = { attributes: true, childList: true, subtree: true };
 observer.observe(document.body, config);
