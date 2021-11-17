e1f3b6f389fe7eefca5fe13bc8710eca8d02bb34
mediv0 <anonymousaxis@gmail.com>
2021-11-17 20:13:16 +0330

    fix: cannot observe data-tribute in shot page

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,28 @@
 import { injectComments } from "./helpers";
 
-const callback: MutationCallback = function (mutationsList, _observer) {
-        // Use traditional 'for loops' for IE 11
-        for (let i = 0; i < mutationsList.length; i++) {
-                // we check if comment section is fully loaded, we inject our comments
-                // this will cause layout shift in the page ( PERFORMANCE ISSUE )
-                if (
-                        mutationsList[i].target.nodeName === "TEXTAREA" &&
-                        mutationsList[i].attributeName === "data-tribute"
-                ) {
-                        injectComments();
-                        break;
+if (document.querySelector(".non-overlay")) {
+        // if user open shot page directly we dont need to observe the page
+        injectComments();
+} else {
+        const callback: MutationCallback = function (mutationsList, _observer) {
+                // Use traditional 'for loops' for IE 11
+                for (let i = 0; i < mutationsList.length; i++) {
+                        // we check if comment section is fully loaded, we inject our comments
+                        // this will cause layout shift in the page ( PERFORMANCE ISSUE )
+                        if (
+                                mutationsList[i].target.nodeName ===
+                                        "TEXTAREA" &&
+                                mutationsList[i].attributeName ===
+                                        "data-tribute"
+                        ) {
+                                injectComments();
+                                break;
+                        }
                 }
-        }
-};
+        };
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-const config = { attributes: true, childList: true, subtree: true };
-observer.observe(document.body, config);
+        // Create an observer instance linked to the callback function
+        const observer = new MutationObserver(callback);
+        const config = { attributes: true, childList: true, subtree: true };
+        observer.observe(document.body, config);
+}
