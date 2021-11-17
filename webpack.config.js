7037b914082a3391e8a48744566a1ef6c2860279
mediv0 <anonymousaxis@gmail.com>
2021-11-17 14:21:54 +0330

    build: zip plugin & banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,47 @@
 const path = require("path");
+const webpack = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
+const PKG = require("./package.json");
 
+const banner = `/*!
+  * {Uribbble} v${PKG.version} 🖖 (https://github.com/persian-tools/vue-persian-tools)
+  * Forged by Mediv0
+  * Released under the MIT License.
+  * © 2021, Mediv0. (https://github.com/mediv0)
+  */`;
 
 module.exports = {
-    entry: {
-        content: "./src/index.ts",
-    },
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                path.resolve(__dirname, "manifest.json"),
-                path.resolve(__dirname, "src", "style.css")
-            ],
-        }),
-    ],
+        entry: {
+                content: "./src/index.ts"
+        },
+        devtool: "inline-source-map",
+        module: {
+                rules: [
+                        {
+                                test: /\.ts?$/,
+                                use: "ts-loader",
+                                exclude: /node_modules/
+                        }
+                ]
+        },
+        resolve: {
+                extensions: [".tsx", ".ts", ".js"]
+        },
+        output: {
+                filename: "[name].js",
+                path: path.resolve(__dirname, "dist")
+        },
+        plugins: [
+                new CopyPlugin({
+                        patterns: [
+                                path.resolve(__dirname, "manifest.json"),
+                                path.resolve(__dirname, "src", "style.css")
+                        ]
+                }),
+                new ZipPlugin({
+                        filename: PKG.name
+                }),
+                new webpack.BannerPlugin({ banner })
+        ]
 };
