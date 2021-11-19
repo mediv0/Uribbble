be9cfd579dd422ea1afbfa2e8261719f73d458a8
mediv0 <anonymousaxis@gmail.com>
2021-11-19 15:03:27 +0330

    fix: component loading fails if user have slow connection

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import { injectComments, observer } from "./helpers";
 // check if we are in shot page
 const shotPage = document.querySelector(".non-overlay") as HTMLDivElement;
 if (shotPage) {
-        // if sidebar is open, we have to manually inject the component then observer for future changes
-        if (document.querySelector(".sidebar-open")) {
-                injectComments();
-        }
-        observer(shotPage);
+        document.onreadystatechange = () => {
+                if (document.readyState === "complete") {
+                        // if sidebar is open, we have to manually inject the component then observer for future changes
+                        document.querySelector(".sidebar-open") &&
+                                injectComments();
+                        observer(shotPage);
+                }
+        };
 } else {
         observer(document.querySelector(".shot-overlay"));
 }
